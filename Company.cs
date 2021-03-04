6c5b911503b8e112641013da15a903d3a167576a
Rhowie86 <ronaldnhowie@gmail.com>
2021-03-04 15:14:24 -0600

    initial commit, assignment completed

---
+++ Company.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace classes
+{
+public class Company
+{
+
+    // Some readonly properties (let's talk about gets, baby)
+    public string Name { get; }
+    public DateTime Founded { get; }
+
+    // Create a public property for holding a list of current employees
+    public List<Employee> currentEmployees { get; set;}
+    /*
+        Create a constructor method that accepts two arguments:
+            1. The name of the company
+            2. The date it was created
+
+        The constructor will set the value of the public properties
+    */
+    public Company(string name, DateTime founded)
+        {
+            Name = name;
+            Founded = founded;
+        }
+
+    public void ListEmployees()
+    {
+        Console.WriteLine();
+        Console.WriteLine($"{Name}");
+        Console.WriteLine($"Currently Employees");
+            foreach (Employee person in currentEmployees)
+            {
+        Console.WriteLine($"{person.FirstName} {person.LastName} as the {person.Title}" );
+
+            }
+    } 
+}
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace classes
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an instance of a company. Name it whatever you like.
+       Company acme = new Company("ACME", DateTime.Now);
+        // Create three employees
+       static List<Employee> GetEmployees()
+       {
+           return new List<Employee>()
+           {
+               new Employee("Dave", "Thomas", "CEO", DateTime.Now),
+               new Employee("Ron", "Howie", "bartender", DateTime.Now),
+               new Employee("Joe", "Biden", "President", DateTime.Now),
+
+           };
+       }
+        // Assign the employees to the company
+        acme.currentEmployees = GetEmployees();
+        acme.ListEmployees();
+        /*
+            Iterate the company's employee list and generate the
+            simple report shown above
+        */
+    }
+}}
